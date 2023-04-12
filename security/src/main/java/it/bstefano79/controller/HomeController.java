af7e37ffa702f295135fda5e1fec886451842dbc
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2023-04-12 14:14:39 +0200

    adds roles in request - response json

--- security/src/main/java/it/bstefano79/controller/HomeController.java
+++ security/src/main/java/it/bstefano79/controller/HomeController.java
@@ -1,5 +1,11 @@
 package it.bstefano79.controller;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
 	@GetMapping("/")
-	  public String privateAccess() {
-	    return "Private Content.";
-	  }
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public String privateAccess() {
+		return "Private Content.";
+	}
 	
-	@GetMapping("/public")
-	  public String allAccess() {
-	    return "Public Content.";
-	  }
+	@GetMapping(path = "/public", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> allAccess() {
+		return ResponseEntity.status(HttpStatus.OK).body(Map.of(
+	            "message", "Questo end-point non ha bisogno dell'autorizzazione"));
+	}
 	
-	@GetMapping("/public/prova")
-	  public String allAccessProva() {
-	    return "Public PROVA Content.";
-	  }
 }
