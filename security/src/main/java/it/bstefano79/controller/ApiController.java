e22965280c256ddc5f8a874e6e93ec04e9f4ecb9
Stefano Belli <56109150+bstefano79@users.noreply.github.com>
2023-04-14 09:52:26 +0200

    get user from token in request

--- security/src/main/java/it/bstefano79/controller/ApiController.java
+++ security/src/main/java/it/bstefano79/controller/ApiController.java
@@ -2,31 +2,51 @@ package it.bstefano79.controller;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.bstefano79.config.jwt.JwtUtils;
+import it.bstefano79.user.details.services.UserDetailsServiceImpl;
+import jakarta.servlet.http.HttpServletRequest;
+
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-
-	@GetMapping("/")
-	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
-	public ResponseEntity<Object> privateAccess() {
-		return ResponseEntity.status(HttpStatus.OK).body(Map.of(
-	            "message", "Questo end-point è privato hai l'autorizzazione"));
-	}
+	
+	@Autowired
+	private JwtUtils jwtUtils;
+	
+	@Autowired
+	private UserDetailsServiceImpl userDetailsService;
 	
 	@GetMapping(path = "/public", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> allAccess() {
+	public ResponseEntity<Object> allAccess(HttpServletRequest request) {
+		String jwt = jwtUtils.getJwtFromCookies(request);
+		UserDetails userDetails=null;
+	    if (jwt != null && jwtUtils.validateJwtToken(jwt)) {
+	    	String username = jwtUtils.getUserNameFromJwtToken(jwt);
+	      
+
+	        userDetails = userDetailsService.loadUserByUsername(username);
+	    }
+	    String message = "Questo end-point non ha bisogno dell'autorizzazione";
+	    if(userDetails==null)
+	    {
+	    	message+=" non sei loggato";
+	    }else {
+	    	message+=" sei loggato come "+userDetails.getUsername();
+	    }
 		return ResponseEntity.status(HttpStatus.OK).body(Map.of(
-	            "message", "Questo end-point non ha bisogno dell'autorizzazione"));
+	            "message", message));
 	}
 	
 	@GetMapping(path = "/public/404", produces = MediaType.APPLICATION_JSON_VALUE)
