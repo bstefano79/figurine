23488718232d426dce3e5c1ee454a98e75cbb626
bstefano79 <bstefano79@gmail.com>
2024-03-11 17:32:15 +0100

    entità per la gestione delle figurine album degli utenti e dello stato che possono avere

---
+++ security/src/main/java/it/bstefano79/entity/FigurineStatus.java
@@ -0,0 +1,41 @@
+package it.bstefano79.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "figurine_statuses")
+public class FigurineStatus {
+	@Id
+    @Column(length = 1)
+    private String id;
+    
+	@NotBlank
+	@NotNull
+	@Size(max = 50)
+    private String description;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
---
+++ security/src/main/java/it/bstefano79/entity/UserAlbum.java
@@ -0,0 +1,49 @@
+package it.bstefano79.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_albums")
+public class UserAlbum {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    
+    @ManyToOne
+    @JoinColumn(name = "album_id", nullable = false)
+    private Album album;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Album getAlbum() {
+		return album;
+	}
+
+	public void setAlbum(Album album) {
+		this.album = album;
+	}
+}
